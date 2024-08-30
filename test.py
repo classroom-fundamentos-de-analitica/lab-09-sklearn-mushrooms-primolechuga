ec1a5dad0e7c0a67f608d65cc07e7e9c581367c9
primolechuga <jucadavida@unal.edu.co>
2024-08-30 10:49:51 -0500

    add notebook

--- test.py
+++ test.py
@@ -48,19 +48,19 @@ def compute_metrics():
     assert estimator is not None, "Model not found"
 
 
-if __name__ == "__main__":
-    run_grading()
-    
+    if __name__ == "__main__":
+        run_grading()
+        
 
-    x_train, x_test, y_true_train, y_true_test = load_datasets()
+        x_train, x_test, y_true_train, y_true_test = load_datasets()
 
-    y_pred_train = estimator.predict(x_train)
-    y_pred_test = estimator.predict(x_test)
+        y_pred_train = estimator.predict(x_train)
+        y_pred_test = estimator.predict(x_test)
 
-    accuracy_train = eval_metrics(y_true_train, y_pred_train)
-    accuracy_test = eval_metrics(y_true_test, y_pred_test)
+        accuracy_train = eval_metrics(y_true_train, y_pred_train)
+        accuracy_test = eval_metrics(y_true_test, y_pred_test)
 
-    return accuracy_train, accuracy_test
+        return accuracy_train, accuracy_test
 
 
 def run_grading():
